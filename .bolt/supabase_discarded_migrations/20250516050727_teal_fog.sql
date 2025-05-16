6381fc5736a13de1e85795fc73c0917b4251f8fa
A6 ❤️‍🩹Lê Văn Tám <a6_lvt_nienkhoa_2024_2028@protonmail.com>
2025-05-16 12:23:41 +0700

    Update PT3

--- .bolt/supabase_discarded_migrations/20250516050727_teal_fog.sql
+++
@@ -1,9 +0,0 @@
-/*
-  # Remove submitter_name from submissions table
-
-  1. Changes
-    - Remove submitter_name column from submissions table
-    - Update existing data to handle the removal
-*/
-
-ALTER TABLE submissions DROP COLUMN submitter_name;
--- supabase/migrations/20250516050924_quiet_shape.sql
+++
@@ -1,9 +0,0 @@
-/*
-  # Make submitter name optional in submissions table
-
-  1. Changes
-    - Alter submissions table to make submitter_name nullable
-*/
-
--- Make submitter_name column nullable
-ALTER TABLE submissions ALTER COLUMN submitter_name DROP NOT NULL;
